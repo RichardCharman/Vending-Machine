d2a2125bb5820b977d66e790259a6ef506f1628b
Richard Charman <richardcharman@live.com>
2015-10-06 14:41:38 +0000

    Set valid and invalid coins, and accept/reject methods

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,7 +1,22 @@
 class VendingMachine
+
+  VALID_COINS = ['5','10','25']
+  attr_reader :display, :coin_return
+
+  def initialize
+    @display = 'INSERT COIN'
+  end
+
   def insert coin
+    if VALID_COINS.include? coin
+      self.display = "#{coin} cents"
+    else
+      self.coin_return = coin
+    end
   end
-  def display
-    '5 cents'
-  end
+
+  private
+
+  attr_writer :display, :coin_return
+
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,8 +1,38 @@
 require 'vending_machine'
 
 describe VendingMachine do
-  it 'accepts coins and updates display' do
-    subject.insert '5'
-    expect(subject.display).to eq '5 cents'
+  context 'valid coins' do
+    it 'accepts a nickel and updates display' do
+      subject.insert '5'
+      expect(subject.display).to eq '5 cents'
+    end
+
+    it 'accepts a dime and updates display' do
+      subject.insert '10'
+      expect(subject.display).to eq '10 cents'
+    end
+
+    it 'accepts a quarter and updates display' do
+      subject.insert '25'
+      expect(subject.display).to eq '25 cents'
+    end
+  end
+
+  context 'invalid coins' do
+    it 'rejects a penny' do
+      subject.insert '1'
+      expect(subject.display).to eq 'INSERT COIN'
+      expect(subject.coin_return).to eq '1'
+    end
+
+    it 'rejects a non-coin' do
+      subject.insert 'acoin45'
+      expect(subject.display).to eq 'INSERT COIN'
+      expect(subject.coin_return).to eq 'acoin45'
+    end
+  end
+
+  it 'displays INSERT COIN when no coins inserted' do
+    expect(subject.display).to eq 'INSERT COIN'
   end
 end
